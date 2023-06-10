f68e880b70e790adef1c56e878c160b9c35cc9c4
ashamweene <82180635+a-mwen@users.noreply.github.com>
2023-06-10 01:30:21 -0400

    Create fibonacci Sequence

---
+++ fibonacci.js
@@ -0,0 +1,16 @@
+
+function fibonacci(n) {
+  const fib = [0, 1];
+
+  for (let i = 2; i < n; i++) {
+    fib[i] = fib[i - 1] + fib[i - 2]
+  }
+  return fib;
+}
+
+console.log(fibonacci(6))// [0,1]
+console.log(fibonacci(3))//[0,1,1]
+console.log(fibonacci(7))// [0,1,1,2,3,5,8]
+console.log(fibonacci(9))// [0,1]
+
+//Big-O = O(n) - As the value of n increases, the number of times line 4 executes also increases.
